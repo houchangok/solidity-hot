f7f638f3f4416fe6a86251dcc9b2bf0f4a674988
houchang123ok@163.com <houchang123ok@163.com>
2025-07-07 21:19:42 +0800

    x

---
+++ sol/revert.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Revert {
+    
+    function revertWithMessage(string memory str) public pure   returns (string memory ){
+   
+   return reverse(str);
+
+    
+}
+ 
+function reverse(string memory _str) public pure returns (string memory) {
+    bytes memory strBytes = bytes(_str);
+    uint256 len = strBytes.length;
+    bytes memory reversed = new bytes(len);
+    
+    for (uint256 i = 0; i < len; i++) {
+        reversed[len - 1 - i] = strBytes[i];
+    }
+    return string(reversed);
+}
+
+}
+
+
